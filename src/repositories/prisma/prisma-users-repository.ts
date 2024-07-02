413c6113acc4e7c48caba76b903e762c53a46512
victor <josevictornascimento2016@gmail.com>
2024-07-02 14:24:11 -0300

    :recycle: refactor: repair project folder pattern

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { Prisma } from '@prisma/client'
-import { prisma } from '../../../lib/prisma'
+import { prisma } from '@/lib/prisma'
 import { UsersRepository } from './../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
