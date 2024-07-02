bd3641d89595277bb82fdedc177d4391d13baade
victor <josevictornascimento2016@gmail.com>
2024-07-02 11:26:12 -0300

    :recycle: refactor: create user already exists error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '../use-cases/register'
 import { PrismaUsersRepository } from './../repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '../use-cases/erros/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,7 +22,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
