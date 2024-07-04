bf6f26b331177eb2ea10937c399f62991a60971f
victor <josevictornascimento2016@gmail.com>
2024-07-04 19:26:28 -0300

    :test_tube: test: create the registration, profile and authenticate tests

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
-import { MakeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,7 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    const registerUseCase = MakeRegisterUseCase()
+    const registerUseCase = makeRegisterUseCase()
 
     await registerUseCase.execute({
       name,
