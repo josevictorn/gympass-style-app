3003a22122cf74b4ec84c8ee423f38d8381bc4ee
victor <josevictornascimento2016@gmail.com>
2024-07-04 18:48:48 -0300

    :sparkles: feat: configure test environment e2e

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      [
+        'src/http/controllers/**',
+        './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+      ],
+    ],
+  },
 })
