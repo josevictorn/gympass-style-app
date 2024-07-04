08cda6b9bb322112662bddb525f2edea21b82860
victor <josevictornascimento2016@gmail.com>
2024-07-04 13:47:24 -0300

    :sparkles: feat: configure jwt in fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/erros/invalid-credentials-error'
-import { MakeAuthenticateUseCase } from '@/use-cases/factories/make-register-use-case copy'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -15,12 +15,23 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const authenticateUseCase = MakeAuthenticateUseCase()
+    const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
